b363da7fab5e9a4fe49a3a59cce2aeedcdd67169
Choong Jin Ng <jin_9393@hotmail.com>
2015-12-21 05:10:39 -0800

    Fixed mail function

--- functions/email.php
+++ functions/email.php
@@ -15,64 +15,16 @@ function sendEmail() {
   $subject = $_POST["subject"];
   $body = $_POST["body"];
   
-  //Create a new PHPMailer instance
-  $mail = new PHPMailer;
+  $subject = "$subject from $name of $university";
 
-  //Tell PHPMailer to use SMTP
-  $mail->isSMTP();
-
-  //Enable SMTP debugging
-  // 0 = off (for production use)
-  // 1 = client messages
-  // 2 = client and server messages
-  $mail->SMTPDebug = 0;
-
-  //Ask for HTML-friendly debug output
-  $mail->Debugoutput = 'html';
-
-  //Set the hostname of the mail server
-  $mail->Host = 'beaubien.web-dns1.com';
-  // use
-  // $mail->Host = gethostbyname('smtp.gmail.com');
-  // if your network does not support SMTP over IPv6
-
-  //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-  $mail->Port = 465;
-
-  //Set the encryption system to use - ssl (deprecated) or tls
-  $mail->SMTPSecure = 'tls';
-
-  //Whether to use SMTP authentication
-  $mail->SMTPAuth = true;
-
-  //Username to use for SMTP authentication - use full email address for gmail
-  $mail->Username = "inquiry@conextus.ca";
-
-  //Password to use for SMTP authentication
-  //Do not hardcode it!!!
-  $mail->Password = "marshmellow";
-
-  //Set who the message is to be sent from
-  $mail->setFrom($email, $name);
-
-  //Set who the message is to be sent to
-  $mail->addAddress('incorporatedconextus@gmail.com', 'Conextus Incorporated');
-  
-  $mail->Subject = "$subject from $name of $university with email '$email'";
-  $subject = "$subject from $name of $university with email '$email'";
-  $mail->Subject = $subject;
-
-  $mail->msgHTML($body, dirname(__FILE__), true); //Create message bodies and embed images
-
-  //Replace the plain text body with one created manually
-  $mail->AltBody = $body;
-
-  //Attach an image file
-  //$mail->addAttachment('images/phpmailer_mini.png');
+  $to = 'incorporatedconextus@gmail.com';
+  $headers = "From: $email" . "\r\n" .
+    "Reply-To: $email" . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
   //send the message, check for errors
-  if (!$mail->send() && !mail($to, $subject, $message, $headers)) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
+  if (!mail($to, $subject, $body, $headers)) {
+    echo 'Sorry! Please email at <a href="mailto:incorporatedconextus@gmail.com">incorporatedconextus@gmail.com</a> while we fixed this issue.';
   }
 
   else {
