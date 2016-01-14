44fc72c09fbfb72aeba73342ee0956af469a5f29
Choong Jin Ng <jin_9393@hotmail.com>
2016-01-14 06:08:15 -0800

    Updated php files

--- index.php
+++ index.php
@@ -9,255 +9,71 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1"/>
-  
-  <link rel="stylesheet" href="css/grid.css"/>
-  <link rel="stylesheet" href="css/main.css"/>
-  <link rel="stylesheet" href="css/media.css"/>
-  <link rel="stylesheet" href="normalize.css"/>
-  
-  <script src="js/jquery-2.1.3.min.js" type="text/javascript" charset="utf-8"></script>
-  <script type="text/javascript" src="js/autoScrollTo.js"></script> <!-- External js from CSS tricks -->
-  <script type="text/javascript" src="js/wufoo.js"></script><!-- External js from wufoo-->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+  <meta charset="utf-8"/>
   <title>Conextus</title>
+  <meta name="description" content="ConextUS, a Friendship application"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <link rel="stylesheet" href="css/ghost-buttons.css"/>
+  <link rel="stylesheet" href="css/media-query.css"/>
+  <!--[if lt IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script><script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->
 </head>
 <body>
-  <!-- HEADER / NAVIGATION -->
-  <div class="header">
-    <div class="grid_100">
-      <ul>
-        <li class="grid_25"><a href="#home" class="topNav">Conextus</a></li>
-        <li class="grid_25"><a href="#application" class="topNav">Application</a></li>
-        <li class="grid_25"><a href="#about" class="topNav">About Us</a></li>
-        <li class="grid_25"><a href="#contact" class="topNav">Contact Us</a></li>
-      </ul>
+  <div class="container1">
+    <h1>Conextus</h1>
+    <p>A buddy for everything!</p>
+
+    <h2 id="gb1">Beta Signup</h2>
+    <a class="ghost-button-semi-transparent" href="registerForm.php">Sign Up</a>
+    <h2 id="gb2"><img src="img/ComingSoon.png"  style="width:320px;height:95px;"> </h2>
+  </div>
+  
+  <div class="container2">
+    <h2 id="gb3">Meet a new buddy every day, and uncover exclusive connections. </h2>
+    <p> Starting post-secondary is one of the biggest steps and challenges in anybody's life. </p>
+    <p>We believe in the difference it can make to somebody if they have a friend to be with for their upcoming adventure. </p>
+    <p>Especially a buddy who likes the same things you do!</p>
+<img src="img/GirlUsingPhone.png"  style="width:auto;height:auto;"/> 
+  </div>
+
+  <div class="container3">
+    <h2 id="gb5">A common interest with a person could lead to a long and meaningful friendship.</h2>
+    <p>My thing is, and I've always been this way - to get to know as many people as I possibly can on a personal level, </p>
+    <p>so that way, when you get on the football field, you've got your buddy right beside you, and you're ready to go.</p>
+    <p>-Russell Wilson</p>
+    <div class="videoContainer">
+      <iframe src="http://www.wideo.co/embed/10823231441775638681?height=400&amp;width=640" frameborder="0" class="appVideo"></iframe>
+    </div>
+    
+    <img src="img/NuwanProfile.png"  style="width:auto;height:auto;"/> 
+  </div>
+  <div class="container4">
+    <h2 id="gb5">Send a message to meet!</h2>
+    <p>The rapport is already there if you have something in common with that person. Say hello and meet up on campus!</p>
+  </div>
+  
+  <div class="container5">
+    <h2 id="gb7">Find Your Buddy!</h2>
+    <a class="ghost-button-semi-transparent" href="registerForm.php"> Early Access Reservation</a>
+    <h2 id="gb8"><img src="img/ComingSoon.png"  style="width:320px;height:95px;"/> </h2>
+  </div>
+
+  <!--FOOTER START -->
+  <div class="footer">
+    <div class="footerContent">
+      <h6>CONNECT WITH US!</h6>
+      <a href="https://www.facebook.com/Conextus-1490009591318369" target="_blank">
+        <img src="img/facebookIcon.png" class="footerIcon"/>
+      </a>
+      <a href="https://twitter.com/ConexterInc" target="_blank">
+        <img src="img/twitterIcon.png" class="footerIcon"/>
+      </a>
+      <a href="https://instagram.com/conextus.inc/" target="_blank">
+         <img src="img/instagramIcon.png" class="footerIcon"/>
+      </a>
+      <p>Copyright © 2016 | Conextus Incorporated | All Rights Reserved.</p>
     </div>
   </div>
-  <!-- HEADER / NAVIGATION END -->
-	
-	
-	<!-- CONTENT -->
-	<div class = "content">
-		
-		<!-- COMPANY IMG -->
-		<div class ="conextus" id = "home">
-			<img src="biglogo.jpg" alt="" class="companyLogo"/>
-		</div>
-		<!-- COMPANY IMG END -->
-	
-		
-		<!-- COMPANY INFO -->
-		<div class = "companyContent">
-			<div class = "company">
-				<div class = "grid_100">
-					<div class = "companyText">
-						<br>
-						<h1>Company</h1>
-						<p>
-						  We are Conextus Incorporated which is a startup founded in July 2015. In Conextus Inc., we specialize in developing smartphone applications specialized as a means for solving major pains and problems prevalent in the lives of our customers. To this end, our debut product Conextus is a smartphone application intended to serve as a means to assist college and university students to discover, network, and plan face-to-face social interactions with students sharing common interests.
-						</p>
-					</div>
-				</div>
-
-				<!-- VIDEO -->
-				<div class = "videoContent">
-					<div class = "video">
-						<div class = "grid_100">
-							<div class = "videoContainer">
-							<iframe src="http://www.wideo.co/embed/10823231441775638681?height=400&width=640" frameborder="0" class="appVideo"></iframe>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<!-- VIDEO END -->
-		</div>
-		<!-- COMPANY INFO END -->
-		
-		
-		
-		<!-- APPLICATION -->
-		<span class = "anchor" id = "application"></span>
-		<div class = "applicationContent">
-			<div class ="app">
-				
-				<div class = "grid_100">
-					<div class="applicationText">
-						<h1>Application</h1>
-						<p>
-						Our debut product, Conextus, has a competitive edge over competitors, like Tinder or WeChat for a number of reasons. First of all, unlike Tinder, Conextus is specifically designed for non-romantic friendships and will be marketed as such. Instead of helping users find a significant other, Conextus  being designed for College undergraduate students in need of a better social life will link users up with multiple fellow students with whom users share common interests. Conextus  also has a menu where users can select activities to do with their potential new friends which are non romantic and suitable for groups of friends. Conextus 's primary means of alleviating loneliness is providing users the means to find compatibility in a group-oriented setting, getting students out of isolation as well as bridging the social and cultural gaps and on campuses. Moreover, unlike WeChat, Conextus  on a specific location, college campuses instead of a wider area like a city meaning users know the area and are familiar with it making them more inclined to find friends and people with similar interests with more predictability for interaction. In Conextus Inc., we believe that the best way to build big is to start small and in a specific fixed location which is applicable to social life. Since Conextus  is focused on a specific area in particular a college or university campus, this means there is a security layer added, a feature absent in more wider oriented apps like WeChat which is users' SFU email ID and its subsequent verification by users through a confirmation message sent to users' emails.
-						</p>
-						<p>
-						At the same time, our competitors in the vein of Tinder and Friendsy are already well established, popular and adapting all the time meaning, they will keep expanding, finding more users and developing new features that have the potential to be appealing to all walks of life thus rivaling or mimicking the features and purpose of our product. Our competitors may react of our product's introduction by expanding physically to Canada and North America to be visible in person and well-established gaining a competitive edge over Conextus Inc., and Project Conextus or adopting similar features as Conextus , which can beat us to popularity given the popularity, constantly changing and well-established nature of competing products like Tinder. Another way competitors can react to our product's introduction is offer to buy our product Conextus  or even acquire our company Conextus Inc., by paying us a huge amount of money in return. At Conextus Inc., we strive to overcome (non) adoption of our company or production Conextus  by providing our customers with a product that possesses features that differ from our competitions and correct common problems found in competing products. These range all the way from a catchy logo design, better security such as proper email verification and scrambled texts for users to enter, a step to ensure that our app has human users only with no bots, this latter point a flaw found in our competitors like Tinder. Lastly, our company will be sensitive and open to our customers' suggestions, complaints, and desires, to this end continuously adapting our product to better serve our customers while providing quality customer service such as but not limited to Questions and Answers, customer outreach events and a physical building for our business so to be recognizable and connect with customers.
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- APPLICATION END -->
-		
-		<!-- DESIGN PROTOTYPE -->
-		<div class = "grid_100">
-			<div class = "designContent">
-				<div class ="designPrototype">
-					<img src="img/design1.png"/>
-				</div>
-			</div>
-		</div>
-		
-		<!-- DESIGN PROTOTYPE END -->
-		
-		<!-- Beta Sign Up -->
-		<div class = "betaSignup">
-			<div class = "registerContent">
-				<div class = "register">
-					<div class ="test">
-						<p class = "registerButton"><a href="registerForm.php" target="_blank">Register for Upcoming Beta!</a></p>
-						<!--
-						<form>
-							<ul class="registerForm">
-								<li>Name</li><input typ"text" name="Name">
-								<li>Email</li><input typ"text" name="Email">
-								<li>University</li><input typ"text" name="University">
-							</ul>
-						</form>
-						<button><a href="" >Register!</a></button>
-						-->
-					</div>
-				</div>
-			</div>
-		</div>
-		<!--Beta Sign Up End-->
-		<!-- TEAM -->
-		<span class = "anchor" id = "about"></span>
-		<div class = "teamContent">
-			<div class ="team">
-				<p class = "teamHeader">MEET THE TEAM</p>
-				<div class = "teamGrid_25">
-					<img src="jul1.jpg"/>
-					<h4 class = "teamName">Julius Michael</h4>
-					<h5 class = "teamTitle">Chief Executive Officer</h5>
-						<p class = "teamDescription">
-						  is a 3rd year Business Administration Student, specializing in Management Information System and Entrepreneurship & Innovation. His mission is to improve the social life of university students and bring the students with similar interests together. He wants the students to enjoy and maximize their time, especially at SFU. 
-						</p>
-					<h5 class = "teamTitle"></h5>
-				</div>
-				
-				<div class = "teamGrid_25">
-					<img src="nuw1.jpg" />
-					<h4 class = "teamName">Nuwan Biyagamage</h4>
-					<h5 class = "teamTitle">Chief Administrative Officer</h5>
-						<p class = "teamDescription">
-						is a 4th year History major at SFU but has an entrepreneurial business mindset. His main interest has been and is innovation and the prospects of app development business as well as e-commerce.
-						</p>
-						 
-				</div>
-				
-				<div class = "teamGrid_25">
-					<img src="me.png" />
-					<h4 class = "teamName">Ken Yuen</h4>
-					<h5 class = "teamTitle">Lead Designer / Marketing Designer</h5>
-						<p class = "teamDescription">
-						is a graduated student from Simon Fraser University with a bachelor of science in <b>Interactive Art and Technology</b> with one year experience in <b>Computer Science</b> background. Currently working with Web Tech Direct, he has experience as a designer, front end developer and data analyst.
-						</p>
-				</div>
-				
-				<div class = "teamGrid_25">
-					<img src="jin1.jpg" />
-					<h4 class = "teamName">Choong Jin Ng</h4>
-					<h5 class = "teamTitle">Chief Technology Officer / Back End Developer</h5>
-						<p class = "teamDescription">
-						is a 3rd year Engineering student, majoring in Computer Engineering and minoring in Computer Science. He wants to focus on software engineering, and enjoys tinkering with computers as much as he learns about them. Jin also enjoys photography, video games and snow boarding.
-						</p>
-				</div>
-				
-				<div class = "teamGrid_25">
-					<img src= "joseph.jpg" />
-					<h4 class = "teamName">Joseph Fredrick</h4>
-					<h5 class = "teamTitle">Public Relations Officer</h5>
-					<p class = "teamDescription">
-					  is a 3rd year student majoring in economics and minoring in business. His passion is creating and innovating new, and cool products. He seeks to improve SFU's social life through new and innovative ways. He is a member of many sfu clubs and volunteers with many charitable organizations.
-					</p>
-				</div>
-				
-				<div class = "teamGrid_25">
-					<img src="hai1.jpg" />
-					<h4 class = "teamName">Haijun Liu</h4>
-					<h5 class = "teamTitle">Marketing Officer</h5>
-					<p class = "teamDescription">is currently a Corporate Sales Consultant at one of Canada's largest telecommunications carrier. He is an aspiring entrepreneur with several years of experience within the direct sales and marketing industry.His mission in any business is based on one goal: To contribute to society by connecting them with the best services and products available.
-					</p>
-				</div>
-				<div class="teamGrid_25">
-					<img src ="" />
-					<h4 class="teamName">Ga Yan Yip</h4>
-					<h5 class="teamTitle">Front End Developer</h5>
-						<p class = "teamDescription">is currently a 2nd year Interactive Arts and Technology student with a passion for design. She enjoys programming and creating visual interfaces. She also likes ice cream, cake, cats, and turtles.
-						</p>
-					</div>
-				<div class="teamGrid_25">
-					<img src= "" />
-					<h4 class="teamName">Andersen Li</h4>
-					<h5 class="teamTitle">Front End Developer</h5>
-					<p class="teamDescription">is a 3rd year Interactive Arts and Technology student. He has an interest in web and application development. His goal is to connect students and create a positive learning experience by getting students to socialize and build connections.
-					</p>
-				</div>
-			</div>
-		</div>
-		<!-- TEAM END -->
-		
-		<!-- EMAIL -->
-		<span class="anchor" id="contact"></span>
-		<div class="contactContent">
-          <div class="contact">
-            <h2>Contact us!</h2>
-            Fill out our <a href="registerForm.php" target="_blank">online form</a>.
-            <br/><br/>
-          </div>
-<!--
-			<div class="contact" id="wufoo-mk46d230mnvaq8">
-			  Fill out our <a href="https://cky4.wufoo.com/forms/mk46d230mnvaq8">online form</a>.
-			</div>
--->
-		</div>
-		<!-- EMAIL END -->
-	</div>
-	<!-- CONTENT END-->
-	
-	<!-- FAQ -->
-<!--
-	<div class="FAQContent">
-		<div class="FAQholder">
-		<div class = "FAQ">
-			<h2>Frequently Asked Questions</h2>
-			<p class="FAQQuestions">Insert question here</p>
-			<p class="FAQAnswers">Insert answer here</p>
-			<p class="FAQQuestions">Insert question here</p>
-			<p class="FAQAnswers">Insert answer here</p>
-			<p class="FAQQuestions">Insert question here</p>
-			<p class="FAQAnswers">Insert answer here</p>
-			<p class="FAQQuestions">Insert question here</p>
-			<p class="FAQAnswers">Insert answer here</p>
-			<p class="FAQQuestions">Insert question here</p>
-			<p class="FAQAnswers">Insert answer here</p>
-			<p class="FAQQuestions">Insert question here</p>
-			<p class="FAQAnswers">Insert answer here</p>
-		</div>
-		</div>
-	</div>
--->
-	<!-- FAQ -->
-	
-	<!-- FOOTER -->
-	<div class="footer">
-		<div class="footerContent">
-			<h6>CONNECT WITH US!</h6>
-			<a href="https://www.facebook.com/Conextus-1490009591318369" target="_blank"><img src="img/facebookIcon.png" class = "footerIcon"/></a>
-			<a href="https://twitter.com/ConexterInc" target="_blank"><img src="img/twitterIcon.png" class = "footerIcon"/></a>
-			<a href="https://instagram.com/conextus.inc/" target="_blank"><img src="img/instagramIcon.png" class = "footerIcon"/></a>
-			<p>Copyright © 2016 | Conextus Incorporated | All Rights Reserved.</p>
-		</div>
-	</div>
-	<!-- FOOTER END -->
+  <!--FOOTER END -->
 </body>
 </html>
--- registerForm.php
+++ registerForm.php
@@ -15,63 +15,63 @@ if (isset($_POST["name"]) &&
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1"/>
-  
-  <link rel="stylesheet" href="css/grid.css"/>
-  <link rel="stylesheet" href="css/main.css"/>
-  <link rel="stylesheet" href="css/media.css"/>
-  <link rel="stylesheet" href="normalize.css"/>
-	
-  <script src="js/jquery-2.1.3.min.js" type="text/javascript" charset="utf-8"></script>
-  <script type="text/javascript" src="js/autoScrollTo.js"></script> <!-- External js from CSS tricks -->
-  <script type="text/javascript" src="js/wufoo.js"></script><!-- External js from wufoo-->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+  <meta charset="utf-8"/>
   <title>Conextus</title>
+  <meta name="description" content="ConextUS, a Friendship application"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <link rel="stylesheet" href="css/ghost-buttons.css"/>
+  <link rel="stylesheet" href="css/media-query.css"/>
+  <!--[if lt IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script><script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->
 </head>
 <body>
-  <!-- HEADER / NAVIGATION -->
-  <div class = "header">
-    <div class="grid_100">
-      <ul>
-        <li class = "grid_25"><a href="index.php" class="topNav">Conextus</a></li>
-      </ul>
-    </div>
+  <div align="Right">
+    <a class="ghost-button-semi-transparent" href="index.html">Conextus</a>
+    <a class="ghost-button-semi-transparent" href="https://www.surveymonkey.com/r/WCFSQS2">Try Manually</a>
   </div>
-  <!-- HEADER / NAVIGATION END -->
+  <div class="container1">
 
-  <!-- FORM -->
-  <div class = "registrationFormContent">
-    <div class ="registrationForm">
-      <h2>Contact Us!</h2>
-      <div class = "grid_100">
-        <form action="registerForm.php" method="post" id="usrform">
-          <ul class="registerForm">
-            <li>Name</li><input type="text" name="name" placeholder="John Adams" required/>
-            <li>Email</li><input type="email" name="email" placeholder="john@email.com" required/>
-            <li>University</li><input type="text" name="university" placeholder="Avery University"/>
-            <li>Subject</li>
-            <input type="radio" name="subject" value="Request for Open Beta"/> Open Beta
-            <input type="radio" name="subject" value="Inquiry"/> Inquiry<br/>
-            <li>Body</li><textarea name="body" form="usrform" rows="4" cols="50" placeholder="I would like to sign up for your open beta." required></textarea>
-          </ul>
-          
-          <input type="submit" name="submit_button" value="Submit"/>
-        </form>
-        <p>If our form is having issues, contact us instead at <a href="mailto:incorporatedconextus@gmail.com">incorporatedconextus@gmail.com</a></p>
-      </div>
+    <h1>We Want To Hear From You!</h1>
+    <p></p>
+
+    <div class="form-style-2">
+      <div class="form-style-2-heading">Provide your information</div>
+      <form action="registerForm.php" method="post">
+        <label for="field1"><span>Name<span class="required">*</span></span>
+          <input type="text" class="input-field" name="name" placeholder="John Smith"/>
+        </label>
+        <label for="field2"><span>Email <span class="required">*</span></span>
+          <input type="text" class="input-field" name="email" placeholder="john@smith.com"/>
+        </label>
+        <label for="field3"><span>Regarding</span>
+          <input type="radio" name="subject" value="Request for Open Beta"/> Open Beta
+          <input type="radio" name="subject" value="Inquiry"/> Other<br/>
+        </label>
+        <label for="field4"><span>Message <span class="required">*</span></span>
+          <textarea name="body" class="textarea-field" placeholder="Hello World!"></textarea>
+        </label>
+        <label><span>&nbsp;</span>
+          <input type="submit" value="Submit"/>
+        </label>
+      </form>
     </div>
   </div>
-  <!-- FORM END -->
 	
-  <!-- FOOTER -->
-  <div class="footer" id="formFooter">
-    <div class = "footerContent">
+  <!--FOOTER START -->
+  <div class="footer">
+    <div class="footerContent">
       <h6>CONNECT WITH US!</h6>
-      <a href="https://www.facebook.com/Conextus-1490009591318369" target="_blank"><img src="img/facebookIcon.png" class = "footerIcon"/></a>
-      <a href="https://twitter.com/ConexterInc" target="_blank"><img src="img/twitterIcon.png" class = "footerIcon"/></a>
-      <a href="https://instagram.com/conextus.inc/" target="_blank"><img src="img/instagramIcon.png" class = "footerIcon"/></a>
+      <a href="https://www.facebook.com/Conextus-1490009591318369" target="_blank">
+        <img src="img/facebookIcon.png" class="footerIcon"/>
+      </a>
+      <a href="https://twitter.com/ConexterInc" target="_blank">
+        <img src="img/twitterIcon.png" class="footerIcon"/>
+      </a>
+      <a href="https://instagram.com/conextus.inc/" target="_blank">
+         <img src="img/instagramIcon.png" class="footerIcon"/>
+      </a>
       <p>Copyright © 2016 | Conextus Incorporated | All Rights Reserved.</p>
     </div>
   </div>
-  <!-- FOOTER END -->
+<!--FOOTER END -->
 </body>
